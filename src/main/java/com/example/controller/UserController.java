21e24e1345c17cfbcf2bcc9f8403b4d71960bcb4
DESKTOP-ALBF240\Ryzen 7 <chanquiaelias8@gmail.com>
2024-08-07 00:42:13 -0300

    add User Controller

---
+++ src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,67 @@
+package com.example.controller;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.model.Role;
+import com.example.model.UserSec;
+import com.example.service.IRoleService;
+import com.example.service.IUserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @Autowired
+    private IUserService userService;
+
+    @Autowired
+    private IRoleService roleService;
+
+    @GetMapping
+    public ResponseEntity<List<UserSec>> getAllUsers() {
+        List<UserSec> users = userService.findAll();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserSec> getUserById(@PathVariable Long id) {
+        Optional<UserSec> user = userService.findById(id);
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<UserSec> createUser(@RequestBody UserSec userSec) {
+
+        Set<Role> roleList = new HashSet<>();
+        Role readRole;
+
+        // Recuperar la Permission/s por su ID
+        for (Role role : userSec.getRolesList()){
+            readRole = roleService.findById(role.getId()).orElse(null);
+            if (readRole != null) {
+                //si encuentro, guardo en la lista
+                roleList.add(readRole);
+            }
+        }
+
+        if (!roleList.isEmpty()) {
+            userSec.setRolesList(roleList);
+
+            UserSec newUser = userService.save(userSec);
+            return ResponseEntity.ok(newUser);
+        }
+        return null;
+    }
+}
