6439f75fe79a627aa76b3d5a3bf48bd49440caf4
DESKTOP-ALBF240\Ryzen 7 <chanquiaelias8@gmail.com>
2024-08-07 00:17:25 -0300

    add PermissionService

---
+++ src/main/java/com/example/service/PermissionService.java
@@ -0,0 +1,45 @@
+package com.example.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.model.Permission;
+import com.example.repository.IPermissionRepository;
+
+
+@Service
+public class PermissionService implements IPermissionService {
+
+    @Autowired
+    private IPermissionRepository permissionRepository;
+
+    @Override
+    public List<Permission> findAll() {
+        return permissionRepository.findAll();
+    }
+
+    @Override
+    public Optional<Permission> findById(Long id) {
+        return permissionRepository.findById(id);
+    }
+
+    @Override
+    public Permission save(Permission permission) {
+        return permissionRepository.save(permission);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        permissionRepository.deleteById(id);
+    }
+
+    @Override
+    public Permission update(Permission permission) {
+        return permissionRepository.save(permission);
+    }
+}
+
+
+
