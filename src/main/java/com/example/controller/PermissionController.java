70730a8e88295cd11720535df60919e83a2e832d
DESKTOP-ALBF240\Ryzen 7 <chanquiaelias8@gmail.com>
2024-08-07 10:53:53 -0300

    update permission controller

--- src/main/java/com/example/controller/PermissionController.java
+++ src/main/java/com/example/controller/PermissionController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +40,21 @@ public class PermissionController {
     public ResponseEntity<Permission> createPermission(@RequestBody Permission permission) {
         Permission newPermission = permissionService.save(permission);
         return ResponseEntity.ok(newPermission);
+    }
+
+    // UPDATE
+    @PutMapping("/{id}")
+    public ResponseEntity<Permission> updatePermission(@PathVariable Long id, @RequestBody Permission permission){
+        Optional<Permission> permissionUpdate = permissionService.findById(id);        
+        permissionUpdate.get().setPermissionName(permission.getPermissionName());
+        return ResponseEntity.ok(permissionService.save(permissionUpdate.get()));
+    }
+
+    // // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deletedLogicalPermission(@PathVariable Long id){
+        permissionService.deleteById(id);
+        return ResponseEntity.ok("Permission deleted successfully");
     }
 
 }
