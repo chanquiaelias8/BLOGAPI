5ea561576f9db4899058e463a3b369ace7e05adb
DESKTOP-ALBF240\Ryzen 7 <chanquiaelias8@gmail.com>
2024-08-07 21:32:25 -0300

    add custom querys

--- src/main/java/com/example/repository/IPermissionRepository.java
+++ src/main/java/com/example/repository/IPermissionRepository.java
@@ -1,9 +1,24 @@
 package com.example.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.example.model.Permission;
 
+import jakarta.transaction.Transactional;
+
 @Repository
 public interface IPermissionRepository extends JpaRepository<Permission, Long>{
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM roles_permissions WHERE permission_id = :permissionId", nativeQuery = true)
+    void deleteRolePermissionsByPermissionId(@Param("permissionId") Long permissionId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM Permission p WHERE p.id = :permissionId")
+    void deleteById(@Param("permissionId") Long permissionId);
 }
