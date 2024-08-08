e33fb031782e23ac549e930f0bbaa6b7519d4c28
DESKTOP-ALBF240\Ryzen 7 <chanquiaelias8@gmail.com>
2024-08-08 17:58:55 -0300

    Role CRUD complete

--- src/main/java/com/example/controller/RoleController.java
+++ src/main/java/com/example/controller/RoleController.java
@@ -2,14 +2,18 @@ package com.example.controller;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -61,5 +65,33 @@ public class RoleController {
         Role newRole = roleService.save(role);
         return ResponseEntity.ok(newRole);
     }
-}
 
+    // UPDATE 
+    @PutMapping("/{id}")
+    public ResponseEntity<Role> updateRole(@PathVariable Long id, @RequestBody Role updateRole){
+        // Encontrar el Role, o lanzar una excepción si no se encuentra
+        Role roleToUpdate = roleService.findById(id)
+                                    .orElseThrow(() -> new RuntimeException("Role not found with id: " + id));
+
+        // Recuperar y asignar los permisos
+        Set<Permission> permiList = updateRole.getPermissionsList().stream()
+            .map(permission -> permiService.findById(permission.getId()).orElse(null))
+            .filter(Objects::nonNull)
+            .collect(Collectors.toSet());
+
+        // Actualizar los campos del Role
+        roleToUpdate.setRole(updateRole.getRole());
+        roleToUpdate.setPermissionsList(permiList);
+
+        // Guardar y retornar la respuesta
+        Role updatedRole = roleService.update(roleToUpdate);
+        return ResponseEntity.ok(updatedRole);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteRole(@PathVariable Long id){
+        roleService.deleteById(id);
+        return ResponseEntity.ok("Permission deleted successfully");
+    }
+}
